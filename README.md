7371de78bac4198125aaec1b48a321096694b591
Bob German <bgerman@BobG.dc540.corp>
2020-10-11 11:50:51 -0400

    Credit where credit is due.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # arduinohidprox
-Interfacing an Arduino with a HID ProPro 5355AGK00 (and likely others)
+Interfacing an Arduino with a HID ProxPro 5355AGK00 (and likely others)
+
+I acquired a HID ProxPro reader, and was interested in using it with Arduino. 
+
+I chose the Arduino UNO, because it can be powered with 12V via the power jack, 
+and I believe it can externally power the reader via its VIN pin.
+
+I found several sources online for interfacing the RFID readers with Arduino. I
+settled on Daniel Smith's project from 2012 on Pagemac.com, because it's the most
+straightforward. Daniel didn't include any license data in the published code,
+so I'm assuming it's open and public domain, and I'm crediting his work here just
+because it's the right thing to do.
+
+http://www.pagemac.com/projects/rfid/arduino_wiegand
+
+As I said, I'm using a ProxPro reader, but it seems like as long as they support 
+Weigand data over the standard DATA0 and DATA1 outputs, they'll probably work.
+
+Tie DATA0 to PIN2, and DATA1 to PIN3, and let her rip.  Debugging data available
+in serial monitor. Will be adding basic display configuration back in as time
+permits.
--- hidprox.ino
+++ hidprox.ino
@@ -1,23 +1,10 @@
 /*
- * HID RFID er Wiegand Interface for Arduino Uno
- * Written by Daniel Smith, 2012.01.30
- * www.pagemac.com
+ * HID RFID Wiegand Interface for Arduino Uno
+ * Originally Written by Daniel Smith, 2012.01.30
+ * Modified by @dc540_nova, 2020-10-10 
  *
- * This program will decode the wiegand data from a HID RFID Reader (or, theoretically,
- * any other device that outputs weigand data).
- * The Wiegand interface has two data lines, DATA0 and DATA1.  These lines are normall held
- * high at 5V.  When a 0 is sent, DATA0 drops to 0V for a few us.  When a 1 is sent, DATA1 drops
- * to 0V for a few us.  There is usually a few ms between the pulses.
- *
- * Your reader should have at least 4 connections (some readers have more).  Connect the Red wire 
- * to 5V.  Connect the black to ground.  Connect the green wire (DATA0) to Digital Pin 2 (INT0).  
- * Connect the white wire (DATA1) to Digital Pin 3 (INT1).  That's it!
- *
- * Operation is simple - each of the data lines are connected to hardware interrupt lines.  When
- * one drops low, an interrupt routine is called and some bits are flipped.  After some time of
- * of not receiving any bits, the Arduino will decode the data.  I've only added the 26 bit and
- * 35 bit formats, but you can easily add more.
- 
+ * Daniel's original work was apparently based on a 5V reader. I used a 12V reader, which changes
+ * some of the elctrical requirements. Everything else works the same.
 */
  
  
